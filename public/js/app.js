d3ca41b1fbd68fe3590e12a0fe14f225467fc43e
Justin Rich <justinrich2008@gmail.com>
2017-08-21 15:35:57 -0700

    added search functionality

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,8 @@ const cardBody =
 '</div>';
 let contactContainer = document.querySelector('.contactContainer');
 let modal = document.querySelector('.modal');
-
+let pplSearch = document.getElementById('pplSearch');
+let results = [];
 
 loadContacts();
 
@@ -31,6 +32,29 @@ window.onload = ()=>{
 		modal.classList.remove('fadeIn');
   }
 
+	pplSearch.addEventListener('keyup',(e)=>{
+		// console.log('tap',e.target.value);
+		let fieldVal = e.target.value;
+		document.querySelector('.contactContainer').innerHTML='';
+		if(fieldVal!=''){
+
+			let newRes = search(fieldVal,results);
+			postToHTML(newRes);
+		}else{
+			postToHTML(results);
+		}
+
+	})
+
+}
+
+//search that uses the searching library 'fuse'
+function search(term,arr){
+	let options ={
+		keys:['name.first','name.last']
+	}
+	let fuse = new Fuse(arr,options);
+	return fuse.search(term);
 }
 
 
@@ -38,42 +62,44 @@ function loadContacts(){
   fetch('https://randomuser.me/api/?results=12&inc=picture,phone,email,name,login,location,nat,dob')
     .then(resp => resp.json())
     .then(json => {
-      let results = json.results;
+			//save results
+			results = json.results;
+			postToHTML(json.results)
+		});
+}
 
-      results.forEach(item=>{
-        console.log(item);
-        let element = document.createElement('div');
-        element.classList.add('card');
-        element.innerHTML = cardBody;
-        let profileImage = element.querySelector('#profileImg');
-            profileImage.src = item.picture.large;
-        let name = element.querySelector('#name');
-            name.innerHTML = item.name.first+' '+item.name.last
-        let email = element.querySelector('#email');
-            email.innerHTML = item.email;
-        let location = element.querySelector('#location');
-            location.innerHTML = item.location.city+', '+ item.nat;
+function postToHTML(json){
+	json.forEach(item=>{
+		console.log(item);
+		let element = document.createElement('div');
+		element.classList.add('card');
+		element.innerHTML = cardBody;
+		let profileImage = element.querySelector('#profileImg');
+				profileImage.src = item.picture.large;
+		let name = element.querySelector('#name');
+				name.innerHTML = item.name.first+' '+item.name.last
+		let email = element.querySelector('#email');
+				email.innerHTML = item.email;
+		let location = element.querySelector('#location');
+				location.innerHTML = item.location.city+', '+ item.nat;
 
-        element.onclick = ()=>{
-          document.querySelector('.modal .img img').src = item.picture.large;
-          document.querySelector('.modal .name').innerHTML = item.name.first+' '+item.name.last;
-          document.querySelector('.modal .email').innerHTML = item.email;
-          document.querySelector('.modal .location').innerHTML = item.location.city;
-					let date = moment(item.dob);
-					document.querySelector('.modal .address').innerHTML =
-						item.location.street+', '+item.location.state+' '+
-						item.location.postcode+' '+item.nat;
-					document.querySelector('.modal .phone').innerHTML = item.phone;
-					document.querySelector('.modal .birthdate').innerHTML = 'Birthday: '+date.format('MM/DD/YY');
-					modal.classList.add('fadeIn');
-
-
-        }
-        console.log(element);
-        contactContainer.append(element);
-
-      });
+		element.onclick = ()=>{
+			document.querySelector('.modal .img img').src = item.picture.large;
+			document.querySelector('.modal .name').innerHTML = item.name.first+' '+item.name.last;
+			document.querySelector('.modal .email').innerHTML = item.email;
+			document.querySelector('.modal .location').innerHTML = item.location.city;
+			let date = moment(item.dob);
+			document.querySelector('.modal .address').innerHTML =
+				item.location.street+', '+item.location.state+' '+
+				item.location.postcode+' '+item.nat;
+			document.querySelector('.modal .phone').innerHTML = item.phone;
+			document.querySelector('.modal .birthdate').innerHTML = 'Birthday: '+date.format('MM/DD/YY');
+			modal.classList.add('fadeIn');
 
 
-    });
+		}
+		console.log(element);
+		contactContainer.append(element);
+
+	});
 }
